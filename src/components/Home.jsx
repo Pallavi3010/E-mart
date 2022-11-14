ca03f9424eb2900b8c3e1bd63b686b46e33fb568
Pallavi3010 <pallaveebhilai3010@gmail.com>
2022-11-14 13:51:20 +0530

    image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="hero">
       <div className="card bg-dark text-white border-0">
-        <img src="/assets/projectcart.jpg" className="card-img" alt="Baclground" height="550"/>
+        <img src="https://szajewski.com/images/Portfolio/20190806/nowe1/FOLIO_house_20150826.jpg" className="card-img" alt="Baclground" height="550"/>
         <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
             <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
